387ca949b805701da3e84fce6721fe95411cca2d
Piotr <dsw55390@student.dsw.edu.pl>
2024-04-23 15:36:46 +0200

    Dodanie logów - 100 razy

--- BASH_L4.sh
+++ BASH_L4.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+# Funkcja do tworzenia 100 plików log
+create_log() {
+   for ((i=1; i<=100; i++))
+   do
+      echo "plik: log$i.txt" >log$i.txt
+      echo "Skrypt: BASH_L4.sh" >>log$i.txt
+      echo "Data: $(date +'%Y-%m-%d')">>log$i.txt
+   done
+}
 
 print_date() {
     echo "Dziś jest: $(date +'%Y-%m-%d')"
 }
 if [ "$1" == "--date" ]; then
     print_date
+elif [ "$1" == "--logs" ]; then
+    create_log
 fi
+
+
+
