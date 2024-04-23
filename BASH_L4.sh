0819f5ea9430ed6ac22c60d7862cd06507d19752
Piotr <dsw55390@student.dsw.edu.pl>
2024-04-23 21:01:32 +0200

    Część 2 projektu - error i f. init

--- BASH_L4.sh
+++ BASH_L4.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
 # Funkcja pomocy
 show_help() {
-    echo "Użycie: ./nazwa_skryptu.sh --date | --logs | --logs <liczba> | --help"
-    echo "--date: Wyświetla dzisiejszą datę."
-    echo "--logs: Tworzy 100 plików log."
-    echo "--logs <liczba>: Tworzy określoną liczbę plików log."
-    echo "--help: Wyświetla pomoc."
+    echo "Użycie: ./nazwa_skryptu.sh --date |-d| --logs |-l| --logs <liczba> | --help |-h| "
+    echo "--date (-d): Wyświetla dzisiejszą datę."
+    echo "--logs (-l): Tworzy 100 plików log."
+    echo "--logs <liczba> (-l <liczba>): Tworzy określoną liczbę plików log."
+    echo "--help (-h): Wyświetla pomoc."
+    echo "--init (-i): Klonowanie repozytorium oraz dodanie ścieżki do PATH"
+    echo "--error (-e): Tworzy 100 plików error w folderach error, z argumentem liczbowym - dowolną ilość"
+}
+# Funkcja do tworzenia plików error w osobnych folderach
+create_error_logs() {
+    for ((i=1; i<=100; i++))
+    do
+        dir="error$i"
+        mkdir -p "$dir"
+        echo "plik: $dir/error$i.txt" > "$dir/error$i.txt"
+        echo "Skrypt: $0" >> "$dir/error$i.txt"
+        echo "Data: $(date +'%Y-%m-%d')" >> "$dir/error$i.txt"
+    done
+}
+create_n_error_logs() {
+    local count=$1
+    for ((i=1; i<=$count; i++))
+    do
+        dir="error$i"
+        mkdir -p "$dir"
+        echo "plik: $dir/error$i.txt" > "$dir/error$i.txt"
+        echo "Skrypt: $0" >> "$dir/error$i.txt"
+        echo "Data: $(date +'%Y-%m-%d')" >> "$dir/error$i.txt"
+    done
 }
 
+# Funkcja inicjująca skrypt
+initialize() {
+    local scripts_dir="$PWD/55390_repo"
+    if [ ! -d "$scripts_dir" ]; then
+        mkdir -p "$scripts_dir"
+        git clone https://github.com/pw55390/Lab4.git "$scripts_dir"
+        echo "Repozytorium zostało sklonowane do $scripts_dir"
+        export PATH="$PATH:$scripts_dir"
+        echo "Dodano ścieżkę do $scripts_dir do zmiennej PATH"
+    else
+        echo "Katalog $scripts_dir już istnieje. Nie wykonano klonowania repozytorium."
+    fi
+}
 # Funkcja do tworzenia określonej liczby plików log
 create_n_log() {
     local count=$1
@@ -33,17 +70,25 @@ print_date() {
     echo "Dziś jest: $(date +'%Y-%m-%d')"
 }
 
-if [ "$1" == "--date" ]; then
+if [[ "$1" == "--date" || "$1" == "-d"  ]]; then
     print_date
-elif [ "$1" == "--logs" ]; then
+elif [[ "$1" == "--logs" || "$1" == "-l"  ]]; then
 	if [ -n "$2" ]; then 
 		create_n_log "$2"
         else
         create_log
 	fi
-elif [ "$1" == "--help" ]; then
+elif [[ "$1" == "--help" || "$1" == "-h"  ]]; then
 show_help
+elif [[ "$1" == "--init" || "$1" == "-i"  ]]; then
+initialize
+elif [[ "$1" == "--error" || "$1" == "-e"  ]]; then
+	if [ -n "$2" ]; then 
+		create_n_error_logs "$2"
+        else
+        create_error_logs
+	fi
 else
-    echo "Niepoprawne użycie. Użyj --help, aby zobaczyć dostępne opcje."
+echo "Niepoprawne użycie. Użyj --help, aby zobaczyć dostępne opcje."
     exit 1
 fi
